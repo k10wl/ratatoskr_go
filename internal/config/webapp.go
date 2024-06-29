2ab2890cdfa8ddea5f2ab08b75eede6a48c5057c
k10wl <aleks.antonsheko@gmail.com>
2024-06-29 23:19:58 +0300

    fix: sticky group name and prevent scroll shift

--- internal/config/webapp.go
+++ internal/config/webapp.go
@@ -15,7 +15,7 @@ type WepAppConfig struct {
 	Token       string
 }
 
-const WebAppVersion = "1.1.2"
+const WebAppVersion = "1.1.3"
 
 func GetWebAppConfig(getenv func(string) string) (*WepAppConfig, error) {
 	stringAdminIDs := getenv("ADMIN_IDS")
